394f86f63f121c01feed931a445c6a00d6b6b3da
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-02-10 09:47:46 +0900

    fix: fix umd banner

--- build.ts
+++ build.ts
@@ -1,24 +1,35 @@
 import * as rolldown from "rolldown";
 import packageJson from "./package.json" with { type: "json" };
+// @ts-expect-error
+import { header } from "rollup-plugin-header";
+// @ts-expect-error
+import { minify as esbuildMinify } from "rollup-plugin-esbuild-minify";
 
 const isWatch = process.argv.includes("--watch");
+const banner = [
+  "/*",
+  " * p5-frame-capturer",
+  ` * ${packageJson.description}`,
+  " *",
+  " * @license MIT",
+  ` * @version ${packageJson.version}`,
+  ` * @repository ${packageJson.repository.url}`,
+  " */",
+  "",
+].join("\n");
 const options = {
   input: "src/umd.ts",
   platform: "browser",
   external: "",
   output: {
-    banner: [
-      "/*",
-      " * p5-frame-capturer",
-      ` * ${packageJson.description}`,
-      " *",
-      " * @license MIT",
-      ` * @version ${packageJson.version}`,
-      ` * @repository ${packageJson.repository.url}`,
-      " */",
-    ].join("\n"),
     format: "umd",
   },
+  plugins: [
+    esbuildMinify(),
+    header({
+      header: banner,
+    }),
+  ],
   treeshake: true,
   define: {
     "process.env.NODE_ENV": JSON.stringify(
